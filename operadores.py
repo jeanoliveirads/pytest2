43491380dcf220f1fae1a3d8b9ecd29ccbb87af1
Jean Oliveira <99375625+jeanoliveirads@users.noreply.github.com>
2023-08-25 14:28:36 -0300

    Update operadores.py

--- operadores.py
+++ operadores.py
@@ -15,18 +15,3 @@ menor_ou_igual = 2 <= 2
 igual = 'a' == 'a'
 diferente = 'a' != 'b'
 print('Olha meu print aqui')
-
-#exercicio com if e comparação
-primeiro_valor = input('Digite um valor: ')
-segundo_valor = input('Digite outro valor: ')
-
-if primeiro_valor >= segundo_valor:
-    print(
-        f'{primeiro_valor=} é maior ou igual '
-        f'ao que {segundo_valor=}'
-    )
-else:
-    print(
-        f'{segundo_valor=} é maior '
-        f'do que {primeiro_valor=}'
-    )
